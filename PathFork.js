d00624162a5d6b3a801f026cfb947a23fd19c706
Colten Jackson <jazzyjackson@gmail.com>
2018-10-03 21:53:42 -0500

    added readme

--- PathFork.js
+++ PathFork.js
@@ -19,29 +19,34 @@ module.exports = class PathFork extends Transflect {
 
     /**
      * child_process.spawn takes a string executable name, an array of arguments, and an options object
+     * @return ${undefined} - child_process is not a stream, so no sense in returning it
      */
     _open(source){
-        return this.fork = child_process.spawn(
+        this.fork = child_process.spawn(
             source.pathname,
             this.arrayify(source.query),
             {
                 env: process.env,
                 cwd: process.cwd()
             }
-            // someow my error handler attached in Transflect doesn't attach syncronously...
-        ).on('error', error => this.destroy(error))
+        )
+        // have to explicitly attach destroy-on-error
+        this.fork.on('error', error => this.destroy(error))
     }
 
+    /**
+     * Keeps a reference to all lines piped to program so you can audit the real input.
+     * Then pipes the request body to the stdin of the ongoing program.
+     */
     _transform(chunk, encoding, done){
         this.stdin = this.stdin.concat(chunk.toString().split('\n'))
         this.fork.stdin.write(chunk) && done() || this.fork.stdin.on('drain', done)
     }
 
     /**
-     * I wonder if I run into a problem here with large bodies,
-     * since I don't consume any output until my program accepts all input
-     * all program output is buffered. So. If I hit the HighWaterMark on stdout,
-     * does that pause the input stream? I know the docs told me about this...
+     * I think I'm at risk of blowing the HighWaterMark on stdout stream,
+     * before attaching a data handler, all program output will be buffered
+     * so if a program outputs more than 16kB... what happens?
      */
     _flush(done){
         this.fork.stdout.on('data', chunk => {
@@ -57,7 +62,9 @@ module.exports = class PathFork extends Transflect {
                 args: this.arrayify(this.source.query),
                 stdin: this.stdin,
                 stdout: this.stdout,
-                stderr: this.stderr
+                stderr: this.stderr,
+                signal,
+                code
             }))
         })
     }
