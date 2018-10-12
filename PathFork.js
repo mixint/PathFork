636039499467eb7aa30b62ccc4f20aca7fc53dee
Colten Jackson <jazzyjackson@gmail.com>
2018-10-12 13:24:21 -0500

    Moved some docstrings around, extracted Array.fromQuery out of the class.

--- PathFork.js
+++ PathFork.js
@@ -2,13 +2,30 @@ let Transflect = require('@mixint/transflect')
 let child_process = require('child_process')
 let os = require('os')
 
-Array.prototype.splitPush = function(Buffer){
-    this.push(...String(Buffer).split(os.EOL))
+/**
+ * @param {(buffer|string)} lines - data input or output from a program that needs to be split into an array of lines
+ * 
+ * String coerces Buffer input into Strings, doesn't affect String input
+ * Splits on /n or /r/n and concatenates new elements to existing array via spread '...' and push multiple arguments.
+ */
+Array.prototype.splitPush = function(lines){
+    this.push(...String(lines).split(os.EOL))
+}
+
+/**
+ * @param {object} queryObject - ParsedMessage.query, used for arguments passed to child_process
+ * @return {array} - an array of strings, `key=value` pairs, sans `=` if lacking a value.
+ * I wonder if there's a way to overload Array.from to transform an object like this...
+ */
+Array.fromQuery = function(query){
+    return Object.entries(query).map(tuple =>
+        tuple[1] ? tuple.join('=') : tuple[0]
+    )
 }
 
 module.exports = class PathFork extends Transflect {
-    constructor(opt){
-        super(opt)
+    constructor(){
+        super()
         /* setting up some arrays to log the output of the child process */
         this.stdio = {
             stdin:  [],
@@ -18,31 +35,20 @@ module.exports = class PathFork extends Transflect {
     }
 
     /**
-     * @param {Object} queryObject - ParsedMessage.query, used for arguments passed to child_process
-     * @return {Array} - an array of strings, `key=value` pairs, sans `=` if lacking a value.
-     */
-    arrayify(queryObject){
-        return Object.entries(queryObject).map(tuple =>
-            tuple[1] ? tuple.join('=') : tuple[0]
-        )
-    }
-
-    /**
      * child_process.spawn takes a string executable name, an array of arguments, and an options object
      * @return {undefined} - child_process is not a stream, 
      * so no sense in returning it, instead we have to manually attach error handler to 
+     *
+     *  have to explicitly attach destroy-on-error, kill subprocess if stream errors.
      */
     _open(source){
-        this.fork = child_process.spawn(
-            source.pathname,
-            this.arrayify(source.query),
-            {
-                env: process.env,
-                cwd: process.cwd()
-            }
-        )
-        // stdout / stdin don't exist yet or what ??
-        // have to explicitly attach destroy-on-error, kill subprocess if stream errors.
+        this.args = Array.fromQuery(source.query)
+
+        this.fork = child_process.spawn(source.pathname, this.args, {
+            env: process.env,  // should there be some other default environment loaded for web requests ?
+            cwd: process.cwd() // thought about sending cwd on querystring but I didn't like it...
+        })
+
         this.on('error', error => this.fork.kill())
         this.fork.on('error', error => this.destroy(error))
     }
@@ -50,10 +56,10 @@ module.exports = class PathFork extends Transflect {
     /**
      * Keeps a reference to all lines piped to program so you can audit the real input.
      * Then pipes the request body to the stdin of the ongoing program.
+     * TODO test this, does a child processes stdin emit drains as expected ?
      */
     _transform(chunk, encoding, done){
         this.stdio.stdin.splitPush(chunk)
-        // TODO test this, does a child processes stdin emit drains as expected ?
         this.fork.stdin.write(chunk) && done() || this.fork.stdin.on('drain', done)
     }
 
@@ -61,6 +67,8 @@ module.exports = class PathFork extends Transflect {
      * I think I'm at risk of blowing the HighWaterMark on stdout stream,
      * before attaching a data handler, all program output will be buffered
      * so if a program outputs more than 16kB... what happens?
+     * TODO is this a race? is there a chance a program will exit before _flush is fired ? probably.
+     * Maybe come back to this with lessons from TeleFork.
      */
     _flush(done){
        this.fork.stdout.on('data', data => {
@@ -70,12 +78,11 @@ module.exports = class PathFork extends Transflect {
         this.fork.stderr.on('data', data => {
             this.stdio.stderr.splitPush(data)
         })
-        // TODO is this a race? is there a chance a program will exit before _flush is fired ? probably.
-        // Maybe come back to this with lessons from TeleFork.
-        this.fork.on('exit', (signal, code) => {
+
+        this.fork.on('exit', (signal, code) => {            
             done(null, JSON.stringify({
                 source: this.source.pathname,
-                args: this.arrayify(this.source.query),
+                args: this.args,
                 stdio: this.stdio,
                 signal,
                 code
